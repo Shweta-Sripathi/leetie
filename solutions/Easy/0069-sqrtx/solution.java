d2ead961917ca065fa9615a9097947f0600dfcc3
Shweta-Sripathi <shwetasripathi@users.noreply.github.com>
2026-09-07 06:32:35 +0000

    leetie: Add 69. Sqrt(x) [Easy] (java)

--- solutions/Easy/0069-sqrtx/solution.java
+++ solutions/Easy/0069-sqrtx/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Binary Search, Newton's Method
 // Link     : https://leetcode.com/problems/sqrtx/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 41720000 (beats 0%)
+// Runtime  : 1 ms (beats 99%)
+// Memory   : 42328000 (beats 90%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
