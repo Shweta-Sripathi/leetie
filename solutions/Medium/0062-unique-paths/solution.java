975d2fd3c57c3c5de7f85471acee51be8f423a13
Shweta-Sripathi <shwetasripathi@users.noreply.github.com>
2026-09-07 06:31:39 +0000

    leetie: Add 62. Unique Paths [Medium] (java)

--- solutions/Medium/0062-unique-paths/solution.java
+++ solutions/Medium/0062-unique-paths/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Dynamic Programming, Combinatorics
 // Link     : https://leetcode.com/problems/unique-paths/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42056000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42220000 (beats 29%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
