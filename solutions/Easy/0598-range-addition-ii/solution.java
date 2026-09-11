5b44bf808fa2fb408029de5d7e5a9a24d62b56ab
Shweta-Sripathi <shwetasripathi@users.noreply.github.com>
2026-09-11 09:05:07 +0000

    leetie: Add 598. Range Addition II [Easy] (java)

--- solutions/Easy/0598-range-addition-ii/solution.java
+++ solutions/Easy/0598-range-addition-ii/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Math
 // Link     : https://leetcode.com/problems/range-addition-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42688000 (beats 0%)
+// Runtime  : 1 ms (beats 31%)
+// Memory   : 48452000 (beats 78%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
