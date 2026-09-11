fc0ad6bdc07353bd87810399229b7ff5132f3184
Shweta-Sripathi <shwetasripathi@users.noreply.github.com>
2026-09-11 08:51:26 +0000

    leetie: Add 507. Perfect Number [Easy] (java)

--- solutions/Easy/0507-perfect-number/solution.java
+++ solutions/Easy/0507-perfect-number/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math
 // Link     : https://leetcode.com/problems/perfect-number/
-// Runtime  : 1 ms (beats 96%)
-// Memory   : 42048000 (beats 62%)
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42016000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
