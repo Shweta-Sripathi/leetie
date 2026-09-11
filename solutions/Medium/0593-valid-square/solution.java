85cc4e14a62741f1dc6cded91634ba9887652978
Shweta-Sripathi <sripathishwe04@gmail.com>
2026-09-11 09:03:31 +0000

    leetie: Add 593. Valid Square [Medium] (java)

--- solutions/Medium/0593-valid-square/solution.java
+++ solutions/Medium/0593-valid-square/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Geometry
 // Link     : https://leetcode.com/problems/valid-square/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42752000 (beats 0%)
+// Runtime  : 1 ms (beats 95%)
+// Memory   : 43128000 (beats 90%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
