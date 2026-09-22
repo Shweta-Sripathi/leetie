12837acb62e108096efa64e7c44589dd684500ef
Shweta-Sripathi <sripathishwe04@gmail.com>
2026-09-22 09:35:18 +0000

    leetie: Add 2578. Split With Minimum Sum [Easy] (java)

--- solutions/Easy/2578-split-with-minimum-sum/solution.java
+++ solutions/Easy/2578-split-with-minimum-sum/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Greedy, Sorting
 // Link     : https://leetcode.com/problems/split-with-minimum-sum/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42024000 (beats 0%)
+// Runtime  : 1 ms (beats 94%)
+// Memory   : 42296000 (beats 56%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
