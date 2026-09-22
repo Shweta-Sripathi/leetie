90714937ca26f38257ed04c62d047c656559546a
Shweta-Sripathi <sripathishwe04@gmail.com>
2026-09-22 09:34:36 +0000

    leetie: Add 2575. Find the Divisibility Array of a String [Medium] (java)

--- solutions/Medium/2575-find-the-divisibility-array-of-a-string/solution.java
+++ solutions/Medium/2575-find-the-divisibility-array-of-a-string/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Math, String
 // Link     : https://leetcode.com/problems/find-the-divisibility-array-of-a-string/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42688000 (beats 0%)
+// Runtime  : 6 ms (beats 100%)
+// Memory   : 81384000 (beats 69%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
