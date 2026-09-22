5350c1bbe8d8b3be9ee94676759fa5d3132cccdf
Shweta-Sripathi <sripathishwe04@gmail.com>
2026-09-22 09:32:29 +0000

    leetie: Add 2566. Maximum Difference by Remapping a Digit [Easy] (java)

--- solutions/Easy/2566-maximum-difference-by-remapping-a-digit/solution.java
+++ solutions/Easy/2566-maximum-difference-by-remapping-a-digit/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Greedy
 // Link     : https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 41900000 (beats 0%)
+// Runtime  : 1 ms (beats 94%)
+// Memory   : 42272000 (beats 66%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
