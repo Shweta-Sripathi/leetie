5faf2ef1493a4bd4f186845238cf4829fab15383
Shweta-Sripathi <shwetasripathi@users.noreply.github.com>
2026-09-25 09:18:20 +0000

    leetie: Add 201. Bitwise AND of Numbers Range [Medium] (java)

--- solutions/Medium/0201-bitwise-and-of-numbers-range/solution.java
+++ solutions/Medium/0201-bitwise-and-of-numbers-range/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Bit Manipulation
 // Link     : https://leetcode.com/problems/bitwise-and-of-numbers-range/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 41668000 (beats 0%)
+// Runtime  : 3 ms (beats 100%)
+// Memory   : 45576000 (beats 95%)
 // Language : java
 // Copyright: (c) 2026 Shweta-Sripathi. All rights reserved.
 // Synced by: leetie
